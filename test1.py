4ed22687de60312c4317a2660800168868ce6e1d
Seojun_Lee <epqlf9999@gmail.com>
2025-03-05 16:07:03 +0900

    first commit

---
+++ test1.py
@@ -0,0 +1,10 @@
+
+
+name = input('이름을 입력하세요: ')
+age = int(input('나이를 입력하세요: '))
+height = float(input('키를 입력하세요: '))
+
+print(f'안녕하세요 {name}님!')
+print(f'나이 : {age}')
+print(f'키 : {height:.2f}cm')
+
